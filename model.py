4f1235f484774070a9e52b5542144875adc9dcb1
lee tanenbaum <leedtan@gmail.com>
2017-10-17 18:41:25 -0400

    working on model. currently inside encoder in dynamic_rnn

---
+++ model.py
@@ -0,0 +1,334 @@
+
+from torch.nn import Parameter
+from functools import wraps
+
+import torch as T
+import torch.nn as NN
+import torch.nn.functional as F
+import torch.nn.init as INIT
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from torch.nn.utils import weight_norm as torch_weight_norm
+
+import numpy.random as RNG
+import tensorflow as TF     # for Tensorboard
+from numpy import rate
+
+
+import argparse, sys, datetime, pickle, os
+
+import matplotlib
+from mailbox import _create_carefully
+import matplotlib.pyplot as PL
+
+from PIL import Image
+
+
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+np.set_printoptions(suppress=True)
+from collections import Counter
+from data_loader_stage1 import *
+
+
+def tovar(*arrs):
+    tensors = [(T.Tensor(a.astype('float32')) if isinstance(a, np.ndarray) else a) for a in arrs]
+    vars_ = [T.autograd.Variable(t) for t in tensors]
+    return vars_[0] if len(vars_) == 1 else vars_
+
+
+def tonumpy(*vars_):
+    arrs = [(v.data.cpu().numpy() if isinstance(v, T.autograd.Variable) else
+             v.cpu().numpy() if T.is_tensor(v) else v) for v in vars_]
+    return arrs[0] if len(arrs) == 1 else arrs
+
+
+def div_roundup(x, d):
+    return (x + d - 1) / d
+def roundup(x, d):
+    return (x + d - 1) / d * d
+
+
+def log_sigmoid(x):
+    return -F.softplus(-x)
+def log_one_minus_sigmoid(x):
+    return -x - F.softplus(-x)
+
+def binary_cross_entropy_with_logits_per_sample(input, target, weight=None):
+    if not target.is_same_size(input):
+        raise ValueError("Target size ({}) must be the same as input size ({})".format(target.size(), input.size()))
+
+    max_val = (-input).clamp(min=0)
+    loss = input - input * target + max_val + ((-max_val).exp() + (-input - max_val).exp()).log()
+
+    if weight is not None:
+        loss = loss * weight
+
+    return loss.sum(1)
+
+
+def advanced_index(t, dim, index):
+    return t.transpose(dim, 0)[index].transpose(dim, 0)
+
+
+def length_mask(size, length):
+    length = tonumpy(length)
+    batch_size = size[0]
+    weight = T.zeros(*size)
+    for i in range(batch_size):
+        weight[i, :length[i]] = 1.
+    weight = tovar(weight)
+    return weight
+
+
+def dynamic_rnn(rnn, seq, length, initial_state):
+    length_sorted, length_sorted_idx = T.sort(length, 0, descending=True)
+    _, length_inverse_idx = T.sort(length_sorted_idx)
+    rnn_in = pack_padded_sequence(
+            advanced_index(seq, 1, length_sorted_idx),
+            tonumpy(length_sorted),
+            )
+    rnn_out, rnn_last_state = rnn(rnn_in, initial_state)
+    rnn_out = pad_packed_sequence(rnn_out)[0]
+    out = advanced_index(rnn_out, 1, length_inverse_idx)
+    if isinstance(rnn_last_state, tuple):
+        state = tuple(advanced_index(s, 1, length_inverse_idx) for s in rnn_last_state)
+    else:
+        state = advanced_index(s, 1, length_inverse_idx)
+
+    return out, state
+
+
+def check_grad(params):
+    for p in params:
+        if p.grad is None:
+            continue
+        g = p.grad.data
+        anynan = (g != g).long().sum()
+        anybig = (g.abs() > 1e+5).long().sum()
+        if anynan or anybig:
+            return False
+    return True
+
+
+def clip_grad(params, clip_norm):
+    norm = np.sqrt(
+            sum(p.grad.data.norm() ** 2
+                for p in params if p.grad is not None
+                )
+            )
+    if norm > clip_norm:
+        for p in params:
+            if p.grad is not None:
+                p.grad /= norm / clip_norm
+    return norm
+
+def init_lstm(lstm):
+    for name, param in lstm.named_parameters():
+        if name.startswith('weight_ih'):
+            INIT.xavier_uniform(param.data)
+        elif name.startswith('weight_hh'):
+            INIT.orthogonal(param.data)
+        elif name.startswith('bias'):
+            INIT.constant(param.data, 0)
+
+def init_weights(module):
+    for name, param in module.named_parameters():
+        if name.find('weight') != -1:
+            INIT.xavier_uniform(param.data)
+        elif name.find('bias') != -1:
+            INIT.constant(param.data, 0)
+
+class Residual(NN.Module):
+    def __init__(self,size, relu = True):
+        NN.Module.__init__(self)
+        self.size = size
+        self.linear = NN.Linear(size, size)
+        if relu:
+            self.relu = NN.LeakyReLU()
+        else:
+            self.relu = False
+
+    def forward(self, x):
+        if self.relu:
+            return self.relu(self.linear(x) + x)
+        else:
+            return self.linear(x) + x
+
+class ConvMask(NN.Module):
+    def __init__(self):
+        NN.Module.__init__(self)
+
+    def forward(self, x):
+        global convlengths
+        mask = length_mask((x.size()[0], x.size()[2]),convlengths).unsqueeze(1)
+        x = x * mask
+        return x
+
+
+class Embedder(NN.Module):
+    def __init__(self,
+                 output_size=128,
+                 word_embed_size=128,
+                 num_layers=1,
+                 num_chars=256,
+                 ):
+        NN.Module.__init__(self)
+        self._output_size = output_size
+        self._char_embed_size = char_embed_size
+        self._num_layers = num_layers
+
+        self.embed = NN.DataParallel(NN.Embedding(num_chars, char_embed_size))
+        self.rnn = NN.LSTM(
+                char_embed_size,
+                output_size // 2,
+                num_layers,
+                bidirectional=True,
+                )
+        init_lstm(self.rnn)
+
+    def forward(self, chars, length):
+        num_layers = self._num_layers
+        batch_size = chars.size()[0]
+        output_size = self._output_size
+
+        embed_seq = self.embed(chars).permute(1, 0, 2)
+        initial_state = (
+                tovar(T.zeros(num_layers * 2, batch_size, output_size // 2)),
+                tovar(T.zeros(num_layers * 2, batch_size, output_size // 2)),
+                )
+        embed, (h, c) = dynamic_rnn(self.rnn, embed_seq, length, initial_state)
+        h = h.permute(1, 0, 2)
+        return h[:, -2:].contiguous().view(batch_size, output_size)
+
+
+
+
+
+class Encoder(NN.Module):
+    def __init__(self,size_usr, size_wd, output_size, num_layers):
+        NN.Module.__init__(self)
+        self._output_size = output_size
+        self._size_wd = size_wd
+        self._size_usr = size_usr
+        self._num_layers = num_layers
+
+        self.rnn = NN.LSTM(
+                size_usr + size_wd,
+                output_size // 2,
+                num_layers,
+                bidirectional=True,
+                )
+        init_lstm(self.rnn)
+
+    def forward(self, wd_emb, usr_emb, length):
+        num_layers = self._num_layers
+        batch_size = wd_emb.size()[0]
+        output_size = self._output_size
+        maxlenbatch = wd_emb.size()[1]
+        #DBG HERE
+        usr_emb = usr_emb.unsqueeze(1)
+        usr_emb = usr_emb.expand(batch_size,maxlenbatch,usr_emb.size()[-1])
+        #wd_emb = wd_emb.permute(1, 0, 2)
+        #Concatenate these
+        embed_seq =  T.cat((usr_emb, wd_emb),2)
+        initial_state = (
+                tovar(T.zeros(num_layers * 2, batch_size, output_size // 2)),
+                tovar(T.zeros(num_layers * 2, batch_size, output_size // 2)),
+                )
+        #embed_seq: 160,93,26 length: 160,output_size: 18, init[0]: 2,160,9
+        embed_seq = embed_seq.permute(1,0,2)
+        embed, (h, c) = dynamic_rnn(self.rnn, embed_seq, length, initial_state)
+        h = h.permute(1, 0, 2)
+        return h[:, -2:].contiguous().view(batch_size, output_size)
+
+class Context(NN.Module):
+    def __init__(self,in_size, context_size, num_layers=1):
+        NN.Module.__init__(self)
+        self._context_size = context_size
+        self._in_size = in_size
+        self._num_layers = num_layers
+
+        self.rnn = NN.LSTM(
+                in_size,
+                context_size,
+                num_layers,
+                bidirectional=False,
+                )
+        init_lstm(self.rnn)
+
+    def forward(self, sent_encodings, length):
+        num_layers = self._num_layers
+        batch_size = sent_encodings.size()[0]
+        context_size = self._context_size
+        initial_state = (
+                tovar(T.zeros(num_layers * 2, batch_size, context_size // 2)),
+                tovar(T.zeros(num_layers * 2, batch_size, context_size // 2)),
+                )
+        embed, (h, c) = dynamic_rnn(self.rnn, sent_encodings, length, initial_state)
+        
+        #Not sure what to return here
+        h = h.permute(1, 0, 2)
+        return h[:, -2:].contiguous().view(batch_size, output_size)
+
+
+
+
+
+
+
+
+dataset = UbuntuDialogDataset(
+        'ubuntu_dialogs_small', 
+        'wordcount.pkl', 'usercount.pkl')
+
+vcb = dataset.vocab
+usrs = dataset.users
+num_usrs = len(usrs)
+vcb_len = len(vcb)
+size_usr = 12
+size_wd = 14
+size_sentence = 18
+size_context = 22
+
+user_emb = NN.Embedding(num_usrs+1, size_usr, padding_idx = 0)
+word_emb = NN.Embedding(vcb_len+1, size_wd, padding_idx = 0)
+enc = Encoder(size_usr, size_wd, size_sentence, num_layers = 1)
+context = Context(size_sentence, size_context, num_layers = 1)
+
+
+dataloader = UbuntuDialogDataLoader(dataset, 16)
+
+for item in dataloader:
+    turns, sentence_lengths_padded, speaker_padded, \
+        addressee_padded, words_padded, words_reverse_padded = item
+    words_padded = T.autograd.Variable(words_padded)
+    words_reverse_padded = T.autograd.Variable(words_reverse_padded)
+    speaker_padded = T.autograd.Variable(speaker_padded)
+    addressee_padded = T.autograd.Variable(addressee_padded)
+    
+    #batch, turns in a sample, words in a message, embedding_dim
+    wds_b = T.stack([word_emb(words_padded[i,:,:]) for i in range(batch_size)])
+    wds_rev_b = T.stack([word_emb(words_reverse_padded[i,:,:]) for i in range(batch_size)])
+    #batch, turns in a sample, embedding_dim
+    usrs_b = user_emb(speaker_padded)
+    addres_b = user_emb(addressee_padded)
+    
+    batch_size = turns.size()[0]
+    max_turns = turns.max()
+    max_words = wds_b.size()[2]
+    
+    encodings = enc(wds_b.view(batch_size * max_turns, max_words, size_wd), 
+                    usrs_b.view(batch_size * max_turns, size_usr), 
+                    sentence_lengths_padded.view(-1))
+    a = 2
+
+# Usage:
+# dataset = UbuntuDialogDataset('../ubuntu-ranking-dataset-creator/src/dialogs', 'wordcount.pkl', 'usercount.pkl')
+# dataloader = UbuntuDialogDataLoader(dataset, 16)
+# for item in dataloader:
+#     turns, sentence_lengths_padded, speaker_padded, addressee_padded, words_padded, words_reverse_padded = item
+#     ...
+#
+# wordcount.pkl and usercount.pkl are generated from stage1 parser.
+# If you want asynchronous data loading, use something like
+# dataloader = UbuntuDialogDataLoader(dataset, 16, 4)
