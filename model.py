cd45a06aa4bbd64357c364830489f8802c4ba5a8
lee tanenbaum <leedtan@gmail.com>
2017-10-22 17:51:28 -0400

    only entry of first four samples

--- model.py
+++ model.py
@@ -318,11 +318,11 @@ while True:
                 itr
                 )
         opt.step()
-        if itr % 100 == 0:
-            prob = decoder(ctx[:,:-1:], wds_b[:,1:,:max_output_words],
-                                 usrs_b[:,1:], sentence_lengths_padded[:,1:]).squeeze()
+        if itr % 10 == 0:
+            prob = decoder(ctx[:4,:-1:], wds_b[:4,1:,:max_output_words],
+                                 usrs_b[:4,1:], sentence_lengths_padded[:4,1:]).squeeze()
             #Entropy defined as H here:https://en.wikipedia.org/wiki/Entropy_(information_theory)
-            mask = mask_4d(prob.size(), turns -1 , sentence_lengths_padded[:,1:])
+            mask = mask_4d(prob.size(), turns[:4] -1 , sentence_lengths_padded[:4,1:])
             Entropy = (prob.exp() * prob * -1) * mask
             Entropy_per_word = Entropy.sum(-1)
             Entropy_per_word = tonumpy(Entropy_per_word)[0]
