39797e8b9ac4c90af23c4ff18b357f2d12d92ac0
lee tanenbaum <leedtan@gmail.com>
2017-10-22 22:29:34 -0400

    adversarial word and user embeddings

--- model.py
+++ model.py
@@ -99,8 +99,48 @@ class Context(NN.Module):
         #h = h.permute(1, 0, 2)
         return embed.view(batch_size, -1, context_size), (h, c)
 
+def adversarial_word_users(wds_b, usrs_b, turns,
+           size_wd,batch_size,size_usr,
+           sentence_lengths_padded, enc, 
+           context,words_padded, decoder, scale = 1e-4):
+    usrs_b = user_emb(speaker_padded)
+        
+    max_turns = turns.max()
+    max_words = wds_b.size()[2]
+    encodings = enc(wds_b.view(batch_size * max_turns, max_words, size_wd),
+                usrs_b.view(batch_size * max_turns, size_usr), 
+                sentence_lengths_padded.view(-1))
+    encodings = encodings.view(batch_size, max_turns, -1)
+    ctx, _ = context(encodings, turns)
+    max_output_words = sentence_lengths_padded[:, 1:].max()
+    words_flat = words_padded[:,1:,:max_output_words].contiguous()
+    # Training:
+    _, log_prob, _ = decoder(ctx[:,:-1:], wds_b[:,1:,:max_output_words],
+                             usrs_b[:,1:], sentence_lengths_padded[:,1:], words_flat)
+    
+    loss = -log_prob
+    wds_adv, usrs_adv = T.autograd.grad(loss, [wds_b, usrs_b], grad_outputs=T.ones(loss.size()), 
+                           create_graph=True, retain_graph=True, only_inputs=True)
+    wds_adv = (wds_adv > 0).type(T.FloatTensor) * scale - (wds_adv < 0).type(T.FloatTensor) * scale
+    usrs_adv = (usrs_adv > 0).type(T.FloatTensor) * scale - (usrs_adv < 0).type(T.FloatTensor) * scale
+    wds_adv, usrs_adv = wds_adv.data, usrs_adv.data
+    return wds_adv, usrs_adv
+    
+    
+    
+    '''
+    cls, _, _, nframes = d(data, data_len, embed_d)
 
-
+    #feature_penalty = [T.pow(r - f,2).mean() for r, f in zip(dists_d, dists_g)]
+    loss = binary_cross_entropy_with_logits_per_sample(cls, target, weight=weight) / nframes.float()
+    # Check gradient w.r.t. generated output occasionally
+    grad = T.autograd.grad(loss, data, grad_outputs=T.ones(loss.size()).cuda(), 
+                           create_graph=True, retain_graph=True, only_inputs=True)[0]
+                           
+    advers = (grad > 0).type(T.FloatTensor) * scale - (grad < 0).type(T.FloatTensor) * scale
+    advers = advers.data
+    return advers
+    '''
 class Decoder(NN.Module):
     def __init__(self,size_usr, size_wd, context_size, num_words, 
                  state_size = None, num_layers=1):
@@ -284,7 +324,9 @@ while True:
         words_padded = tovar(words_padded)
         words_reverse_padded = tovar(words_reverse_padded)
         speaker_padded = tovar(speaker_padded)
-        addressee_padded = tovar(addressee_padded)
+        #SO far not used
+        #addressee_padded = tovar(addressee_padded)
+        #addres_b = user_emb(addressee_padded)
         sentence_lengths_padded = cuda(sentence_lengths_padded)
         turns = cuda(turns)
         
@@ -293,11 +335,19 @@ while True:
         max_words = words_padded.size()[2]
         #batch, turns in a sample, words in a message, embedding_dim
         wds_b = word_emb(words_padded.view(-1, max_words)).view(batch_size, max_turns, max_words, size_wd)
-        wds_rev_b = word_emb(words_reverse_padded.view(-1, max_words)).view(batch_size, max_turns, max_words, size_wd)
+        #SO FAR NOT USED:
+        #wds_rev_b = word_emb(words_reverse_padded.view(-1, max_words)).view(batch_size, max_turns, max_words, size_wd)
         #batch, turns in a sample, embedding_dim
         usrs_b = user_emb(speaker_padded)
-        addres_b = user_emb(addressee_padded)
-        
+        if itr % 4 == 0:
+            wds_adv, usrs_adv = adversarial_word_users(wds_b, usrs_b, turns,
+               size_wd,batch_size,size_usr,
+               sentence_lengths_padded, enc, 
+               context,words_padded, decoder, scale = 1e-4)
+            wds_b = wds_b.detach()
+            usrs_b = usrs_b.detach()
+            wds_b = tovar((wds_b + tovar(wds_adv)).data)
+            usrs_b = tovar((usrs_b + tovar(usrs_adv)).data)
         max_turns = turns.max()
         max_words = wds_b.size()[2]
         encodings = enc(wds_b.view(batch_size * max_turns, max_words, size_wd),
