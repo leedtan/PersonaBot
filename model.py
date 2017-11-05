30523d291fb171a0de88c785886b8286349938c1
lee <leedtan@gmail.com>
2017-11-04 22:10:44 -0400

    debugging

--- model.py
+++ model.py
@@ -1124,6 +1124,7 @@ while True:
         reg_grad_norm = sum(T.norm(v) for v in reg_grads.values()) ** 0.5
 
         loss, grad_norm, reg, reg_grad_norm = tonumpy(loss, grad_norm, reg, reg_grad_norm)
+        print('Grad norm', grad_norm)
         loss, reg = loss[0],reg[0]
         assert np.all(~np.isnan(tonumpy(loss)))
         assert np.all(~np.isnan(tonumpy(reg)))
