c33d754fd0c24a7be6327171d65039d84441c6b4
lee <leedtan@gmail.com>
2017-11-05 20:19:44 -0500

    leakysoftplus reversed

--- modules.py
+++ modules.py
@@ -1,4 +1,4 @@
-\
+
 import torch as T
 import torch.nn as NN
 import torch.nn.functional as F
@@ -43,7 +43,12 @@ def div_roundup(x, d):
     return (x + d - 1) / d
 def roundup(x, d):
     return (x + d - 1) / d * d
+class LeakySoftplusReversed(NN.Module):
+    def __init__(self):
+        NN.Module.__init__(self)
 
+    def forward(self, x):
+        return -F.softplus(-x) + x / 100
 
 def log_sigmoid(x):
     return -F.softplus(-x)
