7ba5dd1684992361f9a643290830b366841ee26b
lee tanenbaum <leedtan@gmail.com>
2017-11-11 11:39:34 -0500

    removed scatterplots cus wtf are those crashes from anyway

--- model.py
+++ model.py
@@ -1438,11 +1438,11 @@ while True:
                 )
             '''
             if args.adversarial_sample == 1 and itr > 1000:
-                add_scatterplot(train_writer, losses=[adv_emb_diffs, adv_sent_diffs, adv_ctx_diffs], 
+                '''add_scatterplot(train_writer, losses=[adv_emb_diffs, adv_sent_diffs, adv_ctx_diffs], 
                                 scales=[adv_emb_scales, adv_sent_scales, adv_ctx_scales], 
                                 names=['embeddings', 'sentence', 'context'], itr = itr, 
                                 log_dir = log_train, tag = 'scatterplot', style=adv_style)
-                
+                '''
                 adv_emb_diffs = []
                 adv_sent_diffs = []
                 adv_ctx_diffs = []
